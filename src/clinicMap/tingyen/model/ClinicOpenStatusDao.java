7edb0560222c5fcb327dfeab07273499d163b641
yen155263 <62281716+yen155263@users.noreply.github.com>
2020-04-22 20:23:13 +0800

    後臺程式碼更新
    更新ClinicOpenStatus Bean,新增 openDescription欄位
    controller程式碼新增

--- src/clinicMap/tingyen/model/ClinicOpenStatusDao.java
+++ src/clinicMap/tingyen/model/ClinicOpenStatusDao.java
@@ -1,4 +1,4 @@
-package clinicMap.tingyen.model;
+package tw.tingyen.model;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -10,24 +10,33 @@ public class ClinicOpenStatusDao implements IClinicOpenStatusDao {
 	@Autowired
 	private SessionFactory sessionFactory;
 	@Override
-	public ClinicOpenStatus updateStatus(String clinicID,boolean openStatus,int currentNum) {
+	public ClinicOpenStatus updateStatus(String clinicID,boolean openStatus,int currentNum,String openDescription) {
 		Session session = sessionFactory.getCurrentSession();
 		if(session.get(ClinicOpenStatus.class,clinicID) == null) {
 			ClinicOpenStatus clinicStatusBean = new ClinicOpenStatus();
 			clinicStatusBean.setClinicID(clinicID);
 			clinicStatusBean.setClinicOpenStatus(openStatus);
 			clinicStatusBean.setClinicCurrentNumber(currentNum);
+			clinicStatusBean.setOpenDescription(openDescription);
 			session.save(clinicStatusBean);
 		}
 		ClinicOpenStatus clinicStatusBean = session.get(ClinicOpenStatus.class,clinicID);
 		clinicStatusBean.setClinicOpenStatus(openStatus);
-		clinicStatusBean.setClinicCurrentNumber(currentNum);		
+		clinicStatusBean.setClinicCurrentNumber(currentNum);
+		clinicStatusBean.setOpenDescription(openDescription);
 		return clinicStatusBean;
 	}
 	@Override
 	public ClinicOpenStatus getCurrentNumber(String clinicID) {
 		Session session = sessionFactory.getCurrentSession();
 		ClinicOpenStatus clinicStatusBean = session.get(ClinicOpenStatus.class,clinicID);
+		return clinicStatusBean;
+	}
+	@Override
+	public ClinicOpenStatus saveCurrentNum(String clinicID, int currentNum) {
+		Session session = sessionFactory.getCurrentSession();
+		ClinicOpenStatus clinicStatusBean = session.get(ClinicOpenStatus.class,clinicID);
+		clinicStatusBean.setClinicCurrentNumber(currentNum);
 		return clinicStatusBean;
 	}
 
--- src/clinicMap/tingyen/model/IClinicOpenStatusService.java
+++ src/clinicMap/tingyen/model/IClinicOpenStatusService.java
@@ -1,6 +1,7 @@
-package clinicMap.tingyen.model;
+package tw.tingyen.model;
 
 public interface IClinicOpenStatusService {
-	public ClinicOpenStatus updateStatus(String clinicID,boolean openStatus,int currentNum);
+	public ClinicOpenStatus updateStatus(String clinicID,boolean openStatus,int currentNum,String openDescription);
 	public ClinicOpenStatus getCurrentNumber(String clinicID);
+	public ClinicOpenStatus saveCurrentNum(String clinicID,int currentNum);
 }
